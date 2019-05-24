64c8ea9786d47fb455378fb056037a6fe4c9323c
Slava Biziuykin <farmik222@yandex.ru>
2019-05-24 22:27:01 +0300

    SecondButton

--- Exercise2/index.js
+++ Exercise2/index.js
@@ -1,19 +1,23 @@
 document.getElementsByClassName("ButtonLink")[0].onclick = function () { window.open("https://vk.com/white_power69", "_blank") }
-function First() {
-    var el = document.querySelector("#Slava");
-    el.href = "./SecondStyle.css";
+document.getElementsByClassName("ButtonStyle")[0].onclick = function () {
+    var el = document.querySelector("Link");
+    if (el.className == "Slava") {
+        el.href = "./SecondStyle.css";
+        el.className = "NotSlava";
+    }
+    else {
+        el.href = "./FirstStyle.css";
+        el.className = "Slava";
+    }
 }
-function Second() {
-    var el = document.querySelector("#Slava");
-    el.href = "./FirstStyle.css";
-}
-document.getElementsByClassName("ButtonStyle")[0].onclick = function(){
-        var el = document.querySelector("#Slava");
-        if(el.href = "./FirstStyle.css"){
-            el.href = "./SecondStyle.css"
-        }
-        else{
-            el.href = "./FirstStyle.css";
-        }
+document.getElementsByClassName("ButtonMaket")[0].onclick = function (){
+    var Cont=document.querySelector(".container")
+    Cont.remove();
+    var Body=document.body;
+    var Div=document.createElement("div");
+    Body.appendChild(Div);
+    Div.className="New";
+    var el = document.querySelector("Link");
+    el.href = "./ThirdStyle.css";
 }
 
